4bde0c706d43434eea578390d96b3e85982810bc
Levi Brown <levigroker@gmail.com>
2018-05-17 14:04:38 -0600

    Copyright update

--- GRKSignatureView/GRKPanGestureRecognizer.m
+++ GRKSignatureView/GRKPanGestureRecognizer.m
@@ -2,7 +2,7 @@
 //  GRKPanGestureRecognizer.m
 //
 //  Created by Levi Brown on 2017/10/2
-//  Copyright (c) 2017 Levi Brown <mailto:levigroker@gmail.com> This work is
+//  Copyright (c) 2017-2018 Levi Brown <mailto:levigroker@gmail.com> This work is
 //  licensed under the Creative Commons Attribution 4.0 International License. To
 //  view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
 //
