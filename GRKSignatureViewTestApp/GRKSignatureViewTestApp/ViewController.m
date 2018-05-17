127aa02eedbeeae9f1abe08f76aea897dcaabc66
Levi Brown <levigroker@gmail.com>
2018-05-17 14:04:03 -0600

    Test app now shows snapshot.

--- GRKSignatureViewTestApp/GRKSignatureViewTestApp/ViewController.m
+++ GRKSignatureViewTestApp/GRKSignatureViewTestApp/ViewController.m
@@ -13,15 +13,25 @@
 
 @property (nonatomic, weak) IBOutlet GRKSignatureView *signatureView;
 @property (nonatomic, weak) IBOutlet UISwitch *longPressSwitch;
+@property (nonatomic, weak) IBOutlet UIView *imageContainerView;
+@property (nonatomic, weak) IBOutlet UIImageView *imageView;
 
 - (IBAction)clearAction:(id)sender;
 - (IBAction)longPressValueChanged:(id)sender;
+- (IBAction)imageTapGesture:(UITapGestureRecognizer *)tap;
 
 @end
 
 @implementation ViewController
 
 #pragma mark - Lifecycle
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	
+	self.imageContainerView.hidden = YES;
+}
 
 - (void)viewWillAppear:(BOOL)animated
 {
@@ -38,10 +48,24 @@
 	[self.signatureView erase];
 }
 
+- (IBAction)snapshotAction:(id)sender
+{
+	NSLog(@"snapshotAction:");
+	UIImage *signature = [self.signatureView signatureImageWithColor:[UIColor redColor]];
+	self.imageView.image = signature;
+	self.imageContainerView.hidden = NO;
+}
+
 - (IBAction)longPressValueChanged:(UISwitch *)sender
 {
 	NSLog(@"longPressValueChanged:");
 	self.signatureView.eraseOnLongPress = sender.on;
+}
+
+- (IBAction)imageTapGesture:(UITapGestureRecognizer *)tap
+{
+	NSLog(@"Image Tap");
+	self.imageContainerView.hidden = YES;
 }
 
 @end
